935b176a9209a26dc06ac726344aab3312cbb414
Robin Fussell <rfussell17@gmail.com>
2020-01-14 18:27:45 -0800

    dropdown added

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,8 +4,6 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-
-
 function Input() {
   return (
     <Container>
@@ -22,16 +20,22 @@ function Input() {
           <p></p>
         </Form.Group>
 
-        <Form.Group controlId="Type">
-          <Form.Label>Type</Form.Label>
-          <Form.Control type="text" placeholder="Enter Payment Type" />
-          <p></p>
-        </Form.Group>
-
         <Form.Group controlId="Amount">
           <Form.Label>Cost</Form.Label>
           <Form.Control type="text" placeholder="Enter Amount" />
           <p></p>
+        </Form.Group>
+
+        <Form.Group controlId="Type">
+          <Form.Label>Type</Form.Label>
+          <select>
+            <option value="PayPal">PayPal</option>
+            <option value="Credit">Credit</option>
+            <option selected value="Debit">
+              Debit
+            </option>
+            <option value="Cash">Cash</option>
+          </select>
         </Form.Group>
         <Button variant="dark" type="submit">
           Submit
