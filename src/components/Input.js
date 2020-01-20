582c5e10c6441ea9f3c4db3fd190641ec3677d8a
Robin Fussell <rfussell17@gmail.com>
2020-01-20 15:15:03 -0800

    handle change added

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,32 +4,36 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+
 function Input() {
   return (
     <Container>
       <Form>
         <Form.Group controlId="item">
           <Form.Label>Item</Form.Label>
-          <Form.Control type="text" placeholder="Enter Item" />
+          <Form.Control type="text" placeholder="Enter Item"
+          onChange={this.handleChange} />
           <p></p>
         </Form.Group>
 
         <Form.Group controlId="date">
           <Form.Label>Date</Form.Label>
-          <Form.Control type="date" placeholder="Enter Date" />
+          <Form.Control type="date" placeholder="Enter Date"
+          onChange={this.handleChange} />
           <p></p>
         </Form.Group>
 
         <Form.Group controlId="amount">
           <Form.Label>Cost</Form.Label>
-          <Form.Control type="text" placeholder="Enter Amount" />
+          <Form.Control type="text" placeholder="Enter Amount
+          onChange={this.handleChange}" />
           <p></p>
         </Form.Group>
 
         <Form.Group controlId="type">
           <Form.Label>Type</Form.Label>
           <br></br>
-          <select>
+          <select onChange={this.handleChange}>
             <option value="paypal">PayPal</option>
             <option value="credit">Credit</option>
             <option value="debit">Debit</option>
