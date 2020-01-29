e3db8e33ef00663b380d30c1512c55fa2efd614f
Robin Fussell <rfussell17@gmail.com>
2020-01-29 13:32:46 -0800

    troubleshooting file structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-=======
-var PORT = process.env.PORT || 5000
 
->>>>>>> parent of 3386ace... deploying to heroku
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./components/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
